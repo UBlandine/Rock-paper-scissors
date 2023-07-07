cc1c8addd1daae180b326d0a31c1677e2c97fb24
UBlandine <ugirablando@gmail.com>
2023-07-07 16:46:28 +0200

    Rock Paper Scissors game

--- JS/script.js
+++ JS/script.js
@@ -1,75 +1,73 @@
-// let playerScore = 0
-// let computerScore = 0
-// const button = document.querySelectorAll('selection')
-
-// function getComputerChoice(){
-//   let choices = ['rock', 'paper', 'scissors']
-//    return choices[Math.floor(Math.random() * choices.length)]
-// }
-
-// function playRound() {
 
 
+const optionBtn = document.querySelectorAll('div.optionBtn button');
+const roundResults = document.querySelector('#roundResults');
+const playerPoints = document.querySelector('#playerScore');
+const computerPoints = document.querySelector('#computerScore');
+const resetBtn = document.querySelector('#reset');
 
+//refresh page for new game
+resetBtn.addEventListener('click',() => location.reload());
+  
+optionBtn.forEach(button => { button.addEventListener('click', getPlayerChoice) });
 
-//  if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-//         (playerSelection == 'scissors' && computerSelection == 'paper') ||
-//         (playerSelection == 'paper' && computerSelection == 'rock')) {
-        
-// }
-// }
-// Function to get the computer's choice
-function getComputerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissors'];
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
+let computerSelection = [{choice: 'Rock', value: 0}, {choice: 'Paper', value: 1}, {choice: 'Scissors', value: 2}];
+let playerScore = 0;
+let compScore = 0;
+let playerChoice;
+
+function computerSelection () {
+  let result = computerSelection[Math.floor(Math.random() * computerSelection.length)];
+  return result;
 }
 
-// Function to determine the winner
+function playRound (playerSelection, computerSelection) {
+  let roundWinCombo = `${playerSelection}-${computerSelection.value}`;
+  let playerWinCombo = ['1-0', '0-2', '2-1'];
 
-function playround(playerSelection, computerSelection) {
+    if (Number(playerSelection) === computerSelection.value) {
+      playerPoints.textContent = ++playerScore
+      computerPoints.textContent = ++compScore
+      roundResults.textContent = "Tie!"
+    }else if (playerWinCombo.includes(roundWinCombo)) {
+        playerPoints.textContent = ++playerScore
+        roundResults.textContent = `You win! ${playerChoice} beats ${computerSelection.choice}`;
+    }else {
+        computerPoints.textContent = ++compScore
+        roundResults.textContent = `You lose! ${computerSelection.choice} beats ${playerChoice}`;
+    }
+ checkWinner();
+}
 
-  let playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound());
+const winnerResults ={
+  computer: ["You Lost the game to a computer!", 'red'],
+  player: ["You Win the game!!!!", 'green'],
+  tie: ["The game is a Tie!", 'blue']
+}
 
-  if (playerSelection === computerSelection) {
-    return 'It\'s a tie!';
-  } else if (
-    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-    (playerSelection === 'Scissors' && computerSelection === 'Paper')
-  ) {
-    return 'You win!';
-  } else {
-    return 'Computer wins!';
+function checkWinner() {
+  if (compScore === 5 || playerScore === 5) {
+    if (compScore === playerScore){
+      updateWinner('tie')
+    }else{
+      let win = `${(compScore > playerScore) ? 'computer' : 'player'}`;
+      updateWinner(win);
+    }
   }
 }
 
-// Get the elements
-const selectionButtons = document.querySelectorAll('.selection');
-const playerSelectionButton = document.getElementById('player');
-const computerSelectionButton = document.getElementById('computer');
-const resultLabel = document.getElementById('result');
-const recallButton = document.getElementById('recall');
+function updateWinner(winner){
+  roundResults.textContent = winnerResults[winner][0];
+  roundResults.style.color = winnerResults[winner][1];
 
-// Event listener for player selection
-selectionButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const playerSelection = button.textContent;
-    const computerSelection = getComputerChoice();
-    const result = determineWinner(playerSelection, computerSelection);
-    
-    playerSelectionButton.textContent = `Player selection: ${playerChoice}`;
-    computerSelectionButton.textContent = `Computer selection: ${computerChoice}`;
-    resultLabel.textContent = `Result: ${result}`;
+  optionBtn.forEach(button => {
+    button.removeEventListener('click', getPlayerChoice);
   });
-});
+}
 
-// Event listener for recall button
-recallButton.addEventListener('click', () => {
-  playerSelectionButton.textContent = 'Player selection';
-  computerSelectionButton.textContent = 'Computer selection';
-  resultLabel.textContent = 'Result';
-});
+function getPlayerChoice(e) {
+  let playerSelection= (e.target.id);
+  playerChoice = e.target.textContent;
+  playRound(playerSelection, computerPlay());
+}
 
